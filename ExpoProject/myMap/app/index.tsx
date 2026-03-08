e712487433ff166fc26a45fea013c1a6a8438b1a
Dayastan <dayastant@gmail.com>
2026-03-08 19:19:25 +0530

    Add filesystem docs

--- ExpoProject/myMap/app/index.tsx
+++ ExpoProject/myMap/app/index.tsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, View, Alert } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View, Alert, TouchableOpacity } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Region } from 'react-native-maps';
 import * as Location from 'expo-location';
+import { Ionicons } from '@expo/vector-icons';
+import * as FileSystem from 'expo-file-system/legacy';
+
+import {shareAsync} from 'expo-sharing';
 
 const INITIAL_REGION = {
   latitude: 37.78825,
@@ -11,14 +15,59 @@ const INITIAL_REGION = {
 };
 
 const Page = () => {
-  const [region, setRegion] = useState(INITIAL_REGION);
+  const [region, setRegion] = useState<Region>(INITIAL_REGION);
   const [locationGranted, setLocationGranted] = useState(false);
   const mapRef = useRef<MapView>(null);
+
+  // Update region when map moves
+  const onRegionChangeComplete = (newRegion: Region) => {
+    setRegion(newRegion);
+  };
+
+  // Focus map to New York
+  const focusMap = () => {
+    const newYorkRegion = {
+      latitude: 40.7128,
+      longitude: -74.0060,
+      latitudeDelta: 2,
+      longitudeDelta: 2,
+    };
+
+    mapRef.current?.animateToRegion(newYorkRegion, 1000);
+  };
+
+  const takeSnapshotAndShare = async () => {
+    const snapshot = await mapRef.current?.takeSnapshot({
+      width: 300,
+      height: 300,
+      result: 'base64',
+    });
+
+    const uri = FileSystem.documentDirectory + 'map-snapshot.png';
+    await FileSystem.writeAsStringAsync(uri, snapshot!, {
+       encoding: FileSystem.EncodingType.Base64 
+      });
+    await shareAsync(uri);
+
+  };
+
+  // Zoom into current location
+  const zoom = () => {
+    const zoomRegion = {
+      latitude: region.latitude,
+      longitude: region.longitude,
+      latitudeDelta: 0.01,
+      longitudeDelta: 0.01,
+    };
+
+    mapRef.current?.animateToRegion(zoomRegion, 1000);
+  };
 
   // Request location permission
   useEffect(() => {
     (async () => {
       const { status } = await Location.requestForegroundPermissionsAsync();
+
       if (status !== 'granted') {
         Alert.alert(
           'Permission Denied',
@@ -26,25 +75,63 @@ const Page = () => {
         );
         return;
       }
+
       setLocationGranted(true);
     })();
   }, []);
 
   return (
-    <View style={{ flex: 1 }}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        mapType="standard"
-        style={StyleSheet.absoluteFill}
-        showsUserLocation={locationGranted}
-        showsMyLocationButton={locationGranted} // Android only
-        zoomControlEnabled
-        rotateEnabled={false}
-        region={region}
-        onRegionChangeComplete={(r) => setRegion(r)}
-      />
-    </View>
+    <>
+      <View style={{ flex: 1 }}>
+        <MapView
+          ref={mapRef}
+          provider={PROVIDER_GOOGLE}
+          mapType="standard"
+          style={StyleSheet.absoluteFill}
+          showsUserLocation={locationGranted}
+          showsMyLocationButton={locationGranted}
+          rotateEnabled={false}
+          region={region}
+          onRegionChangeComplete={onRegionChangeComplete}
+        />
+      </View>
+
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.btn} onPress={focusMap}>
+          <Ionicons name="business" size={24} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn} onPress={zoom}>
+          <Ionicons name="earth" size={24} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn} onPress={takeSnapshotAndShare}>
+          <Ionicons name="camera" size={24} />
+        </TouchableOpacity>
+
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    position: 'absolute',
+    top: 15,
+    left: 20,
+    gap: 10,
+  },
+
+  btn: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
 
 export default Page;
